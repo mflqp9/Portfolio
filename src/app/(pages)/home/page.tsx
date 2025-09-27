662e2d3d250ccc57ec73c77bb4ddb0f80976d86d
Muhammad Faisal <mflqp9@gmail.com>
2025-09-27 17:58:27 +0500

    contact + nav 3

--- src/app/(pages)/home/page.tsx
+++ src/app/(pages)/home/page.tsx
@@ -1,12 +1,9 @@
+"use client"
 import HeroSection from '@/components/hero/HeroSection';
 import React from 'react';
 import { choosenTheme } from '@/components/util/theme' ;
 import Skills from '@/components/skills/Skills';
 
-export const metadata={
-title:"My Portfolio ",
-description:"Portfolio Muhammad Faisal"
-}
 
 const HomePage = () => {
 

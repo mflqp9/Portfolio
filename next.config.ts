da00adb0796dcf62c114d73e970fa22ebf97ef81
Muhammad Faisal <mflqp9@gmail.com>
2025-12-07 02:00:16 +0500

    upadate next ver

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  
+  output: "standalone",
+  //allowedDevOrigins: ["http://localhost:3000", "http://localhost:3001"],
 };
 
 export default nextConfig;
